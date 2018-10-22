98f6103af7be4d4d9b27e4edfe7b31bc5ff454f9
Tim Ruffing <crypto@timruffing.de>
2018-10-22 16:25:26 +0200

    Switch to a single malloc call

--- src/ecmult_gen.h
+++ src/ecmult_gen.h
@@ -30,9 +30,8 @@ typedef struct {
 
 static size_t secp256k1_ecmult_gen_context_prealloc_size(void);
 static void secp256k1_ecmult_gen_context_init(secp256k1_ecmult_gen_context* ctx);
-static void secp256k1_ecmult_gen_context_build(secp256k1_ecmult_gen_context* ctx, const secp256k1_callback* cb);
-static void secp256k1_ecmult_gen_context_clone(secp256k1_ecmult_gen_context *dst,
-                                               const secp256k1_ecmult_gen_context* src, const secp256k1_callback* cb);
+static void secp256k1_ecmult_gen_context_build(secp256k1_ecmult_gen_context* ctx, void **prealloc);
+static void secp256k1_ecmult_gen_context_finalize_memcpy(secp256k1_ecmult_gen_context *dst, const secp256k1_ecmult_gen_context* src);
 static void secp256k1_ecmult_gen_context_clear(secp256k1_ecmult_gen_context* ctx);
 static int secp256k1_ecmult_gen_context_is_built(const secp256k1_ecmult_gen_context* ctx);
 
