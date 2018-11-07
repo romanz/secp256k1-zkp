5e6de4caad4e1b9d07776becaa1bdf06e50f5996
Roman Zeyde <me@romanzey.de>
2018-11-07 22:16:58 +0200

    Remove fprintf() and abort()

--- src/util.h
+++ src/util.h
@@ -13,7 +13,6 @@
 
 #include <stdlib.h>
 #include <stdint.h>
-#include <stdio.h>
 
 typedef struct {
     void (*fn)(const char *text, void* data);
@@ -24,15 +23,12 @@ static SECP256K1_INLINE void secp256k1_callback_call(const secp256k1_callback * 
     cb->fn(text, (void*)cb->data);
 }
 
+/* TODO: panic.*/
 #ifdef DETERMINISTIC
 #define TEST_FAILURE(msg) do { \
-    fprintf(stderr, "%s\n", msg); \
-    abort(); \
 } while(0);
 #else
 #define TEST_FAILURE(msg) do { \
-    fprintf(stderr, "%s:%d: %s\n", __FILE__, __LINE__, msg); \
-    abort(); \
 } while(0)
 #endif
 
